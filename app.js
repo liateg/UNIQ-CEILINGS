578d055382081bb72e6a3361f77145eca1f77b9d
liateg <liyategared7@gmail.com>
2026-03-05 21:11:58 +0300

    final

--- app.js
+++ app.js
@@ -266,7 +266,7 @@ document.addEventListener("DOMContentLoaded", () => {
                   <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                 </svg>
               </a>
-              <a href="#" class="social-icon" aria-label="YouTube">
+              <a href="https://www.youtube.com/@UniqCeilings" target="_blank" class="social-icon" aria-label="YouTube">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.42a2.78 2.78 0 0 0-1.94 2C1 8.14 1 12 1 12s0 3.86.42 5.58a2.78 2.78 0 0 0 1.94 2c1.72.42 8.6.42 8.6.42s6.88 0 8.6-.42a2.78 2.78 0 0 0 1.94-2C23 15.86 23 12 23 12s0-3.86-.42-5.58z"></path>
                   <polygon points="9.75 15.02 15.5 12 9.75 8.98 9.75 15.02"></polygon>
